a2696be87e0827d75784730f64bdd2e5f201d3d3
Csajbok-Remenyi Laszlo <csajbokremenyilaszlo@ktch.hu>
2023-04-25 19:47:01 +0200

    Test Weight első verziója

--- test/TestWeight.java
+++ test/TestWeight.java
@@ -1,11 +1,35 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import controllers.MainController;
 
 public class TestWeight {
-    
+    MainController mc;
+
+    @BeforeEach
+    public void initEach() {
+        mc = new MainController();
+    }
+
     @Test
-    public void testValami() {
-        assertEquals(1, 1);
+    public void testCalcWeight01() {
+        double actual = mc.calcWeight(30, 35);
+        double expected = 2.6521848951755493;
+        assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+
+    public void testCalclWeigth02() {
+        double actual = mc.calcWeight(130, 135);
+        double expected = 192.0939631220005;
+        assertEquals(expected, actual, 0.01);
+    }
+
+    @Test
+    public void testCalclWeigth03() {
+        double actual = mc.calcWeight(150, 160);
+        double expected = 303.1068451629199;
+        assertEquals(expected, actual, 0.01);
     }
 }
